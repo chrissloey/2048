e8a04d53b09411749c2c89c93d5d43aba0142341
Chris Sloey <chris.sloey@gmail.com>
2014-04-06 22:33:42 +0100

    Update item values table to use images from current game mode

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	var self = this;
 	self.setGameMode = function(mode) {
-		$('.game-container').removeClass('ad');
-		$('.game-container').removeClass('ap');
-		$('.game-container').removeClass('pro');
-		$('.game-container').addClass(mode);
+		$('.container').removeClass('ad');
+		$('.container').removeClass('ap');
+		$('.container').removeClass('pro');
+		$('.container').addClass(mode);
 
 		if (typeof ga !== "undefined") {
 			ga("send", "event", "game", mode);
